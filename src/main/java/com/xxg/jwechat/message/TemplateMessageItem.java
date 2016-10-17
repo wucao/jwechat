dd1ca3974965613f172a37cf9f7c3f0881cffa38
吴操 <wucao@qiyi.com>
2016-10-17 11:07:41 +0800

    模板消息发送

--- src/main/java/com/xxg/jwechat/message/TemplateMessageItem.java
+++ src/main/java/com/xxg/jwechat/message/TemplateMessageItem.java
@@ -8,19 +8,21 @@ public class TemplateMessageItem {
     private String value;
     private String color;
 
+    public TemplateMessageItem(String value, String color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public TemplateMessageItem(String value) {
+        this.value = value;
+        this.color = "#173177";
+    }
+
     public String getValue() {
         return value;
     }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 }
